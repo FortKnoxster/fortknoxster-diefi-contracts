9d7d8db296b07b79bee8258ed248f724f32096c7
mickey <mj@fortknoxster.com>
2022-07-08 01:38:30 +0200

    upgraded deployment scripts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network) {
+  console.log('network', network)
+  if (network == "development") {
+    console.log('Skipping migrations "1_initial_migration"...')
+    return; // test maintains own contracts
+  }
   deployer.deploy(Migrations);
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,21 @@
 const DieFiPolicy = artifacts.require('DieFiPolicy')
 const DieFiForwarder = artifacts.require('DieFiForwarder')
 
-module.exports = async function (deployer) {
-  await deployer.deploy(DieFiForwarder)
-  const forwarder = await DieFiForwarder.deployed()
-  await deployer.deploy(DieFiPolicy, forwarder.address)
+module.exports = async function (deployer, network) {
+    if (network == "development") {
+        console.log('Skipping migrations "2_deploy_contracts"...')
+        return; // test maintains own contracts
+    }
+    
+    console.log('Deploying DieFiForwarder ...')
+    await deployer.deploy(DieFiForwarder)
+    const forwarder = await DieFiForwarder.deployed()
+    console.log('DieFiForwarder deployed ...')
+
+    console.log('Deploying DieFiPolicy ...')
+    await deployer.deploy(DieFiPolicy, forwarder.address)
+    const policy = await DieFiPolicy.deployed()
+    console.log('DieFiPolicy deployed ...')
+    await policy.initialize('0xb9015d7B35Ce7c81ddE38eF7136Baa3B1044f313')
+    console.log('DieFiPolicy initialized ...')
 }
