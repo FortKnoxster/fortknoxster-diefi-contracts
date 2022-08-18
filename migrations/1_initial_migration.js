799ef428722340e8d174c01d37296d884575849f
mickey <mj@fortknoxster.com>
2022-08-18 19:24:33 +0200

    cleanup

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,6 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer, network) {
-  console.log('network', network)
   if (network == "development") {
     console.log('Skipping migrations "1_initial_migration"...')
     return; // test maintains own contracts
